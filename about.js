3f74ab178450f90cdb6da7b7a783a7910262e8d0
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-03-10 23:59:02 -0500

    changed quotation marks

--- about.js
+++ about.js
@@ -1,11 +1,11 @@
-var strings = [ "natalie braginsky", "music", "poetry", "art", "\"an angry trans shit\"", "\"a markov chain\"" ];
+var strings = [ 'natalie braginsky', 'music', 'poetry', 'art', '\"an angry trans shit\"', '\"a markov chain\"' ];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(0);
 
 	fill(255);
-	textFont("Menlo");
+	textFont('Menlo');
 	textAlign(CENTER, CENTER);
 
 	for (var i = 0; i < strings.length; i++) {
