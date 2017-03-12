d58e221018caff3a27e893557b532f551c71b253
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-03-12 18:16:47 -0400

    add solarized

--- about.js
+++ about.js
@@ -1,12 +1,35 @@
+var randomItem = require('random-item');
+
 var margin = 50;
 var strings = [ 'natalie braginsky', 'music', 'poetry', 'art', 'code', '\"an angry trans shit\"', '\"a markov chain\"' ];
+
+var solarized = {
+	'base03':  '#002b36',
+	'base02':  '#073642',
+	'base01':  '#586e75',
+	'base00':  '#657b83',
+	'base0':   '#839496',
+	'base1':   '#93a1a1',
+	'base2':   '#eee8d5',
+	'base3':   '#fdf6e3',
+	'yellow':  '#b58900',
+	'orange':  '#cb4b16',
+	'red':     '#dc322f',
+	'magenta': '#d33682',
+	'violet':  '#6c71c4',
+	'blue':    '#268bd2',
+	'cyan':    '#2aa198',
+	'green':   '#859900'
+};
+
+var solarizedAccent = ['yellow', 'orange', 'red', 'magenta', 'violet', 'blue', 'cyan', 'green'];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noCursor();
-	background('#002b36');
+	background(solarized.base03);
 
-	fill('#b58900');
+	fill(255);
 	textFont('Menlo');
 	textAlign(CENTER, CENTER);
 
