2dbb43f43d618a29fd2acc5b9a3182bb7f18da40
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-03-13 20:50:11 -0400

    attempt to read file

--- hd.js
+++ hd.js
@@ -1,7 +1,7 @@
 var $ = require("jquery");
-var interleave = require('loose-interleave');
-var Chance = require('chance');
-var chance = new Chance();
+// var interleave = require('loose-interleave');
+// var Chance = require('chance');
+// var chance = new Chance();
 
 $.get("files/some imagist poets.txt", function(corpus) {
 	callback();
