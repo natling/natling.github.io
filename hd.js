46d429cf7c3f9d6725f9dbb158bcbf69de6131f1
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-03-13 20:25:50 -0400

    attempt to read file

--- hd.js
+++ hd.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+var $ = require("jquery");
+// var fs = require('fs');
 // var read = require('read-file');
 // var interleave = require('loose-interleave');
 // var Chance = require('chance');
@@ -9,11 +10,29 @@ var fs = require('fs');
 // 	var reader = new FileReader();
 // 	reader.onload = function() {
 // 		corpus = reader.result;
-// 	}
+// 		console.log(reader.result.substring(0, 200));
+// 	};
 // 	reader.readAsText("files/some imagist poets.txt");
-// }
+// };
 
 // console.log(corpus);
 
 // var corpus = read.sync('files/some imagist poets.txt');
 // console.log(corpus);
+
+// console.log("hue");
+
+// function loadFile(filePath) {
+// 	var result = null;
+// 	var xmlhttp = new XMLHttpRequest();
+// 	xmlhttp.open("GET", filePath, false);
+// 	xmlhttp.send();
+// 	if (xmlhttp.status == 200) {
+// 		result = xmlhttp.responseText;
+// 	}
+// 	return result;
+// }
+
+// loadFile("files/some imagist poets.txt");
+
+$.get("files/some imagist poets.txt");
