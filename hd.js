5288c6069cfa041e6b7406415a5d124aab63b9e0
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-03-13 20:45:36 -0400

    attempt to read file

--- hd.js
+++ hd.js
@@ -1,44 +1,12 @@
 var $ = require("jquery");
-// var fs = require('fs');
-// var read = require('read-file');
-// var interleave = require('loose-interleave');
-// var Chance = require('chance');
-// var chance = new Chance();
+var interleave = require('loose-interleave');
+var Chance = require('chance');
+var chance = new Chance();
 
-// var openFile = function(event) {
-// 	var input = event.target;
-// 	var reader = new FileReader();
-// 	reader.onload = function() {
-// 		corpus = reader.result;
-// 		console.log(reader.result.substring(0, 200));
-// 	};
-// 	reader.readAsText("files/some imagist poets.txt");
-// };
-
-// console.log(corpus);
-
-// var corpus = read.sync('files/some imagist poets.txt');
-// console.log(corpus);
-
-// console.log("hue");
-
-// function loadFile(filePath) {
-// 	var result = null;
-// 	var xmlhttp = new XMLHttpRequest();
-// 	xmlhttp.open("GET", filePath, false);
-// 	xmlhttp.send();
-// 	if (xmlhttp.status == 200) {
-// 		result = xmlhttp.responseText;
-// 	}
-// 	return result;
-// }
-
-// loadFile("files/some imagist poets.txt");
-var corpus = "";
-
-$.get("files/some imagist poets.txt", function(data) {
-	corpus = data;
-	// console.log(data);
+$.get("files/some imagist poets.txt", function(corpus) {
+	callback();
 });
 
-console.log(corpus);
+function callback() {
+	console.log(corpus);
+}
