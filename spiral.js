abc3afc4cae8c7809811ef744124e67d4c2d304b
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-03-16 00:18:38 -0400

    changed behavior

--- spiral.js
+++ spiral.js
@@ -1,12 +1,25 @@
-var a = 2;
-var b = 0.0007;
+var aLow  = 0.01,
+	aHigh = 10,
+	bLow  = 0.0004,
+	bHigh = 0.001,
+
+	resolution = 128,
+
+	aStep = (aHigh - aLow) / resolution,
+	bStep = (bHigh - bLow) / resolution,
+
+	aUp = true,
+	bUp = true;
+
+var a, b;
 
 var n = 30000;
 
 var e = 2.71828;
 
 var density = 1.0;
-var characterArray = [];
+// var characterArray = [];
+var colorArray = [];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -18,13 +31,19 @@ function setup() {
 	textAlign(CENTER, CENTER);
 	fill('#00f72c');
 
-	for (var i = 0; i < n; i++) {
-		if (random() < density) {
-			characterArray.push(String.fromCharCode(random(32, 127)));
-		} else {
-			characterArray.push(' ');
-		}
-	}
+	a = map(0.5, 0, 1, aLow, aHigh);
+	b = map(0.5, 0, 1, bLow, bHigh);
+
+	// a = aLow;
+	// b = bLow;
+
+	// for (var i = 0; i < n; i++) {
+	// 	if (random() < density) {
+	// 		characterArray.push(String.fromCharCode(random(32, 127)));
+	// 	} else {
+	// 		characterArray.push(' ');
+	// 	}
+	// }
 }
 
 function draw() {
@@ -34,14 +53,42 @@ function draw() {
 		var x = a * e ** (b * i) * cos(i);
 		var y = a * e ** (b * i) * sin(i);
 
-		text(characterArray[i], width / 2 + x, height / 2 + y);
-		// text(String.fromCharCode(random(32, 127)), width / 2 + x, height / 2 + y);
+		// text(characterArray[i], width / 2 + x, height / 2 + y);
+		text(String.fromCharCode(random(32, 127)), width / 2 + x, height / 2 + y);
 	}
 
-	characterArray = arrayRotate(characterArray, 1, true);
+	// characterArray = arrayRotate(characterArray, 1, true);
 
-	a = randomWalk(a, 0.01, 4, 0.01);
-	b = randomWalk(b, 0.0004, 0.001, 0.0001);
+	// a = walk(a, aLow, aHigh, aStep, aUp);
+	// b = walk(b, bLow, bHigh, bStep, bUp);
+
+	if (aUp) {
+		if (a < aHigh) {
+			a += aStep;
+		} else {
+			aUp = ! aUp;
+		}
+	} else {
+		if (a > aLow) {
+			a -= aStep;
+		} else {
+			aUp = ! aUp;
+		}
+	}
+
+	if (bUp) {
+		if (b < bHigh) {
+			b += bStep;
+		} else {
+			bUp = ! bUp;
+		}
+	} else {
+		if (b > aLow) {
+			b -= bStep;
+		} else {
+			bUp = ! bUp;
+		}
+	}
 }
 
 function arrayRotate(array, distance, direction) {
@@ -55,11 +102,28 @@ function arrayRotate(array, distance, direction) {
 	return array;
 }
 
-function randomWalk(start, low, high, step) {
-	while (true) {
-		var newStart = start + int(random(-(step + 1), (step + 1)));
-		if (newStart >= low && newStart <= high) {
-			return newStart;
-		}
-	}
-}
+// function randomWalk(start, low, high, step) {
+// 	while (true) {
+// 		var newStart = start + int(random(-(step + 1), (step + 1)));
+// 		if (newStart >= low && newStart <= high) {
+// 			return newStart;
+// 		}
+// 	}
+// }
+
+// function walk(currentValue, low, high, step, direction) {
+// 	if (direction) {
+// 		if (currentValue < high) {
+// 			newValue = currentValue + step;
+// 		} else {
+// 			direction = ! direction;
+// 		}
+// 	} else {
+// 		if (currentValue > low) {
+// 			newValue = currentValue - step;
+// 		} else {
+// 			direction = ! direction;
+// 		}
+// 	}
+// 	return newValue;
+// }
