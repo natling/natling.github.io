97dff98547cf77ab3930dcfdac7d0c17dd0890b3
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-03-17 14:38:31 -0400

    format links

--- directory.js
+++ directory.js
@@ -1,13 +1,18 @@
 var links = ['evaluation', 'okc', 'pronouns', 'love', 'why', 'you'];
 var strings = ['evaluation', 'okc', 'pronouns', 'things we say around love', 'why', 'you'];
 
+var layerHeight;
+
 var canvas;
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 
+	layerHeight = height / links.length;
+
 	for (var i = 0; i < strings.length; i++) {
-		var text = createDiv("<a href=" + links[i] + ">" + strings[i] + "</a>");
-		text.position(100, (i + 1) * 100);
+		var link = createDiv("<section class=\"text\"><p><a href=" + links[i] + ">" + strings[i] + "</a></p></section>");
+		link.position(0, i * layerHeight);
+		link.size(width, layerHeight);
 	}
 }
