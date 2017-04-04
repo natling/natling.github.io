7b48d7f6660ba44a08ba174d6485ee70dbd19247
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-04-04 13:05:58 -0400

    create streams.js

---
+++ streams.js
@@ -0,0 +1,68 @@
+numberOfStreams = 40;
+probabilityOfMoving = 0.9;
+
+lineArray = [], locationsOfStreams = [];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	noCursor();
+	background('#000000');
+
+	textFont('Menlo');
+	textAlign(LEFT, TOP);
+	fill('#00f72c');
+
+	columnWidth = Math.round(textWidth(' '));
+	rowHeight = 11;
+
+	columns = Math.floor(width / columnWidth);
+	rows = Math.floor(height / rowHeight);
+
+	var emptyLine = new Array(columns);
+	emptyLine.fill(' ');
+
+	for (var i = 0; i < rows; i++) {
+		lineArray.push(emptyLine);
+	}
+
+	for (var i = 0; i < numberOfStreams; i++) {
+		locationsOfStreams[i] = int(random(columns));
+	}
+}
+
+function draw() {
+	background('#000000');
+
+	for (var j = 0; j < rows; j++) {
+		for (var i = 0; i < columns; i++) {
+			text(lineArray[j][i], i * columnWidth, j * rowHeight);
+		}
+	}
+
+	var newLine = new Array(columns);
+	newLine.fill(' ');
+
+	for (var i = 0; i < numberOfStreams; i++) {
+		newLine[locationsOfStreams[i]] = String.fromCharCode(random(32, 127));
+		locationsOfStreams[i] = walk(locationsOfStreams[i], 0.9, 0, columns - 1);
+	}
+
+	lineArray.shift();
+	lineArray.push(newLine)
+}
+
+function walk(start, p, low, high) {
+	while (true) {
+		newStart = start;
+		if (random() < p) {
+			if (random() < 0.5) {
+				newStart++;
+			} else {
+				newStart--;
+			}
+		}
+		if (newStart >= low && newStart <= high) {
+			return newStart;
+		}
+	}
+}
