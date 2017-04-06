c8961d84facb75f51548efb9d24f6639df80866e
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-04-06 01:55:05 -0400

    data project

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+var margin = 50;
+
 function preload() {
 	table = loadTable('files/DS_U.S._Port_Calls_2012.csv', 'csv');
 }
@@ -6,4 +8,9 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noCursor();
 	background(0);
+}
+
+function drawAxes() {
+	line(150,height-50,width-50,height-50);
+	line(150,50,150,height-50);
 }
