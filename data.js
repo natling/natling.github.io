4b44fd08bd7ee43ad6fe810679656a2138c23942
nataliebraginsky <nataliebraginskymusic@gmail.com>
2017-04-06 18:17:13 -0400

    data project

--- data.js
+++ data.js
@@ -119,6 +119,7 @@ function drawLabels() {
 
 	textAlign(CENTER, CENTER);
 	text(title, 0, 0, width, marginTop);
+	text(xLabel, 0, height - marginBottom, width, height - marginBottom);
 }
 
 function windowResized() {
