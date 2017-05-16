4068bda7eba160c597f605da73d05ef2e95bf39e
Natalie Braginsky <hi@natalie.computer>
2017-05-16 17:50:23 -0400

    add shuffleArray function

--- libraries/natalie.js
+++ libraries/natalie.js
@@ -9,3 +9,13 @@ function coin(p) {
 		return false
 	}
 }
+
+function shuffleArray(array) {
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	return array;
+}
