f363f4daaef9fe2c5ff5ce39aae9d1f3cf6a135b
Natalie Braginsky <hi@natalie.computer>
2017-05-21 14:54:37 -0400

    add AnimatedBezierCurve class

--- drawing.js
+++ drawing.js
@@ -82,6 +82,61 @@ class AnimatedCurve {
 	}
 }
 
+class AnimatedBezierCurve {
+
+	constructor(x1p, y1p, x1c, y1c, x2c, y2c, x2p, y2p, t1, t2, weight, col, resolution) {
+		this.x1p        = x1p;
+		this.y1p        = y1p;
+		this.x1c        = x1c;
+		this.y1c        = y1c;
+		this.x2c        = x2c;
+		this.y2c        = y2c;
+		this.x2p        = x2p;
+		this.y2p        = y2p;
+		this.t1         = t1;
+		this.t2         = t2;
+		this.weight     = weight;
+		this.col        = col;
+		this.resolution = resolution;
+	}
+
+	draw() {
+		if (t >= this.t2) {
+			noFill();
+			stroke(this.col);
+			strokeWeight(this.weight);
+
+			curve(this.x1p, this.y1p, this.x1c, this.y1c, this.x2c, this.y2c, this.x2p, this.y2p);
+
+		} else {
+
+			if (t >= this.t1) {
+				var progress = map(t, this.t1, this.t2, 0, 1);
+
+				var ax = this.x1p;
+				var bx = this.x1c;
+				var cx = this.x2c;
+				var dx = this.x2p;
+
+				var ay = this.y1p;
+				var by = this.y1c;
+				var cy = this.y2c;
+				var dy = this.y2p;
+
+				for (var i = 0; i < progress; i += this.resolution) {
+					var xTemp = curvePoint(ax, bx, cx, dx, i);
+					var yTemp = curvePoint(ay, by, cy, dy, i);
+
+					fill(this.col);
+					noStroke();
+
+					ellipse(xTemp, yTemp, this.weight, this.weight);
+				}
+			}
+		}
+	}
+}
+
 class AnimatedArc {
 
 	constructor(x, y, w, h, start, stop, direction, t1, t2, weight, col) {
@@ -127,7 +182,10 @@ class AnimatedArc {
 // var numberOfCurves = 0;
 // var curvesArray = [];
 
-// var numberOfArcs = 100;
+// var numberOfBezierCurves = 100;
+// var bezierCurvesArray = [];
+
+// var numberOfArcs = 0;
 // var arcsArray = [];
 
 // function setup() {
@@ -165,6 +223,24 @@ class AnimatedArc {
 // 		curvesArray.push(new AnimatedCurve(x1c, y1c, x1p, y1p, x2p, y2p, x2c, y2c, t1, t2, weight, col, resolution));
 // 	}
 
+// 	for (var i = 0; i < numberOfBezierCurves; i++) {
+// 		var x1c        = random(width);
+// 		var y1c        = random(height);
+// 		var x1p        = random(width);
+// 		var y1p        = random(height);
+// 		var x2p        = random(width);
+// 		var y2p        = random(height);
+// 		var x2c        = random(width);
+// 		var y2c        = random(height);
+// 		var t1         = i * 50;
+// 		var t2         = t1 + 50;
+// 		var weight     = 1;
+// 		var col        = color(255, 255, 255);
+// 		var resolution = 0.001;
+
+// 		curvesArray.push(new AnimatedBezierCurve(x1c, y1c, x1p, y1p, x2p, y2p, x2c, y2c, t1, t2, weight, col, resolution));
+// 	}
+
 // 	for (var i = 0; i < numberOfArcs; i++) {
 // 		var x         = random(width);
 // 		var y         = random(height);
@@ -190,6 +266,10 @@ class AnimatedArc {
 // 	}
 
 // 	for (var i = 0; i < numberOfCurves; i++) {
+// 		curvesArray[i].draw();
+// 	}
+
+// 	for (var i = 0; i < numberOfBezierCurves; i++) {
 // 		curvesArray[i].draw();
 // 	}
 
