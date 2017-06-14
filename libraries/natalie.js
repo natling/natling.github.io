fdb0c3ebde4beddaa780299d1c325ffd083bcea7
Natalie Braginsky <hi@natalie.computer>
2017-06-14 16:08:49 -0400

    add randomFloat()

--- libraries/natalie.js
+++ libraries/natalie.js
@@ -4,6 +4,10 @@ function randomIntegerInclusive(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function randomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 function coin(p) {
 	if (Math.random() < p) {
 		return true
