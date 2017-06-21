ae25c78ad7e004759757c2b877a98937491a8447
Natalie Braginsky <hi@natalie.computer>
2017-06-21 19:23:31 -0400

    create things-we-say-around-love

---
+++ things-we-say-around-love.js
@@ -0,0 +1,43 @@
+var poem = `           and            and
+           and            for
+           and            so
+           and            very
+       because            so
+everybody says           
+ everyone says           
+      how much           
+                          a lot
+                          all the
+                          and i
+                          and miss
+                          and you
+                          as a
+                          because you
+                          for it
+                          from the
+                          i love
+                          just like
+                          like a
+                          more than
+                          so much
+                          to death
+                          too much
+                          very much
+                          with all
+         i say           
+       i think           
+     know that           
+       much as           
+     of course           
+ remember that           
+      tell you           
+          that            and
+       the way           
+  this because           
+        to say           
+       you and           
+   you because           
+      you know           
+      you that           `;
+
+var stringArray = poem.split('\n');
