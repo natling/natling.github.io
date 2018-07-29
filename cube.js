30c1214cf2baaacf267984d3d8ba5c9786ae832a
Natalie Braginsky <hi@natalie.computer>
2018-07-28 20:50:43 -0400

    create cube

---
+++ cube.js
@@ -0,0 +1,24 @@
+var n = 4;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+	background(0);
+
+	for (var y = 0; y < n; y++) {
+		for (var x = 0; x < n; x++) {
+			rotateX(frameCount * 0.001 * 0.5);
+			rotateY(frameCount * 0.002 * 0.5);
+			rotateZ(frameCount * 0.003 * 0.5);
+
+			normalMaterial();
+			box(Math.min(width, height) * 0.4);
+		}
+	}
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
