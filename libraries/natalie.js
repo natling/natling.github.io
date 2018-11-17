b7cabd33635f2629f5a6e30d4cbb38a0135cee6c
Natalie Braginsky <hi@natalie.computer>
2018-11-17 11:58:16 -0500

    modernize

--- libraries/natalie.js
+++ libraries/natalie.js
@@ -1,18 +1,18 @@
-function randomIntegerInclusive(min, max) {
+const randomIntegerInclusive = (min, max) => {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomFloat(min, max) {
+const randomFloat = (min, max) => {
 	return Math.random() * (max - min) + min;
 }
 
-function getBaseLog(x, y) {
-	return Math.log(y) / Math.log(x);
-}
+const mod = (x, n) => (x % n + n) % n
 
-function roundTo(n, digits) {
+const getBaseLog = (x, y) => Math.log(y) / Math.log(x)
+
+const roundTo = (n, digits) => {
 	if (digits === undefined) {
 		digits = 0;
 	}
@@ -22,19 +22,17 @@ function roundTo(n, digits) {
 	return Math.round(n) / multiplicator;
 }
 
-function linlin(value, inMin, inMax, outMin, outMax) {
+const linlin = (value, inMin, inMax, outMin, outMax) => {
 	return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
 }
 
-function coin(p) {
-	return Math.random() < p;
-}
+const coin = p => Math.random() < p
 
-function weightedAverage(values, weights) {
+const weightedAverage = (values, weights) => {
 	var weightedValuesSum = 0;
 	var weightsSum = 0;
 
-	for (var i = 0; i < values.length; i++) {
+	for (let i = 0; i < values.length; i++) {
 		weightedValuesSum += values[i] * weights[i];
 		weightsSum += weights[i];
 	}
@@ -42,12 +40,12 @@ function weightedAverage(values, weights) {
 	return weightedValuesSum / weightsSum;
 }
 
-function randomItem(array) {
+const randomItem = array => {
 	return array[Math.floor(Math.random() * array.length)];
 }
 
-function rotateArray(array, distance, direction) {
-	for (var i = 0; i < distance; i++) {
+const rotateArray = (array, distance, direction) => {
+	for (let i = 0; i < distance; i++) {
 		if (direction) {
 			array.unshift(array.pop());
 		} else {
@@ -57,8 +55,8 @@ function rotateArray(array, distance, direction) {
 	return array;
 }
 
-function shuffleArray(array) {
-	for (var i = array.length - 1; i > 0; i--) {
+const shuffleArray = array => {
+	for (let i = array.length - 1; i > 0; i--) {
 		var j = Math.floor(Math.random() * (i + 1));
 		var temp = array[i];
 		array[i] = array[j];
@@ -67,17 +65,17 @@ function shuffleArray(array) {
 	return array;
 }
 
-function pairwiseDo(array, func) {
-	for (var i = 0; i < array.length - 1; i++) {
+const pairwiseDo = (array, func) => {
+	for (let i = 0; i < array.length - 1; i++) {
 		func(array[i], array[i + 1], i);
 	}
 }
 
-function separateArray(array, func = function() {return true}) {
+const separateArray = (array, func = () => true) => {
 	var separatedArray = [];
 	var subArray = [];
 
-	pairwiseDo(array, function(a, b, i) {
+	pairwiseDo(array, (a, b, i) => {
 		subArray.push(a);
 
 		if (func(a, b, i)) {
@@ -92,23 +90,21 @@ function separateArray(array, func = function() {return true}) {
 	return separatedArray;
 }
 
-function create2DArray(rows, columns) {
+const create2DArray = (rows, columns) => {
 	var x = new Array(rows);
 
-	for (var i = 0; i < rows; i++) {
+	for (let i = 0; i < rows; i++) {
 		x[i] = new Array(columns);
 	}
 
 	return x;
 }
 
-function last(array) {
-	return array[array.length - 1];
-}
+const last = array => array[array.length - 1]
 
-function flatten(ary, ret) {
+const flatten = (ary, ret) => {
 	ret = ret === undefined ? [] : ret;
-	for (var i = 0; i < ary.length; i++) {
+	for (let i = 0; i < ary.length; i++) {
 		if (Array.isArray(ary[i])) {
 			flatten(ary[i], ret);
 		} else {
@@ -118,26 +114,24 @@ function flatten(ary, ret) {
 	return ret;
 }
 
-function interleave () {
+const interleave = () => {
 	var arrs = [].slice.call(arguments);
-	var maxLength = Math.max.apply(Math, arrs.map(function (arr) {
-		return arr.length;
-	}))
+	var maxLength = Math.max.apply(Math, arrs.map(arr => arr.length));
 
 	var result = [];
 
-	for (var i = 0; i < maxLength; ++i) {
-		arrs.forEach(function (arr) {
+	for (let i = 0; i < maxLength; ++i) {
+		arrs.forEach(arr => {
 			if (arr.length > i) {
 				result.push(arr[i]);
 			}
-		})
+		});
 	}
 
 	return result;
 }
 
-function combinations(array, k) {
+const combinations = (array, k) => {
 	if (k == 0) {
 		return [[]];
 	}
@@ -149,7 +143,7 @@ function combinations(array, k) {
 	var output = [];
 	var subset = combinations(array.slice(1), k - 1);
 
-	for (var i = 0; i < subset.length; i++) {
+	for (let i = 0; i < subset.length; i++) {
 		output.push([array[0]].concat(subset[i]));
 	}
 
@@ -158,20 +152,20 @@ function combinations(array, k) {
 	return output;
 }
 
-function permutations(array) {
+const permutations = array => {
 	if (array.length == 1) {
 		return array;
 	}
 
 	var output = [];
 
-	for (var i = 0; i < array.length; i++) {
+	for (let i = 0; i < array.length; i++) {
 		var first = array[i];
 		var rest  = array.slice(0, i).concat(array.slice(i + 1));
 
 		var innerPermutations = permutations(rest);
 
-		for (var j = 0; j < innerPermutations.length; j++) {
+		for (let j = 0; j < innerPermutations.length; j++) {
 			output.push(([first].concat(innerPermutations[j])));
 		}
 	}
@@ -179,7 +173,7 @@ function permutations(array) {
 	return output;
 }
 
-function randomWalkInteger(start, low, high, step) {
+const randomWalkInteger = (start, low, high, step) => {
 	while (true) {
 		var newStart = start + randomIntegerInclusive(-step, step);
 		if (newStart >= low && newStart <= high) {
@@ -188,7 +182,7 @@ function randomWalkInteger(start, low, high, step) {
 	}
 }
 
-function randomWalkFloat(start, low, high, step) {
+const randomWalkFloat = (start, low, high, step) => {
 	while (true) {
 		var newStart = start + randomFloat(-step, step);
 		if (newStart >= low && newStart <= high) {
@@ -197,7 +191,7 @@ function randomWalkFloat(start, low, high, step) {
 	}
 }
 
-function getCharacterDimensions(character) {
+const getCharacterDimensions = character => {
 	var p = document.createElement('p');
 	p.appendChild(document.createTextNode(character));
 	document.body.appendChild(p);
