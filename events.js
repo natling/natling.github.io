a92aa6a09c610b903dd3e129e4f5f37926e0ab12
Natalie Braginsky <hi@natalie.computer>
2019-04-05 16:43:50 -0400

    create events page

---
+++ events.js
@@ -0,0 +1,23 @@
+const events = [
+	{
+		time     : new Date(2019, 5, 7, 20),
+		main     : {
+			name : 'William Kurtis Chang / Daniel Lawrence / elska / natalie[dot]computer',
+		},
+		location : {
+			name : 'Magick City',
+			link : 'http://magick.city',
+		},
+	},
+
+	{
+		time     : new Date(2019, 5, 9, 18),
+		main     : {
+			name : 'Shepherdess premieres new works by Mary Prescott, Viola Yip, and natalie[dot]computer',
+		},
+		location : {
+			name : 'Areté Venue and Gallery',
+			link : 'https://www.aretevenue.com',
+		},
+	},
+];
